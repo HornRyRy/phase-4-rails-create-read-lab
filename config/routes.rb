cdc678e28a8778fd6964124bc41ca79048b60963
Ryan Horn <rj.horn@gmail.com>
2023-02-05 18:43:07 -0700

    ryry commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  resources :plants
+
+  get "/plants", to: "application#index"
+  get "/plants/:id", to: "application#show"
+  get 
+  
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
