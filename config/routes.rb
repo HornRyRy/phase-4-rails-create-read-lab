4dc7e9eb327742f57918e35acd6a6ecefb8fa611
Ryan Horn <rj.horn@gmail.com>
2023-02-06 08:37:36 -0700

    assignment done

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,3 @@
-Rails.application.routes.draw do
-  resources :plants
-
-  get "/plants", to: "application#index"
-  get "/plants/:id", to: "application#show"
-  get 
-  
-
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+Rails.application.routes.draw do 
+  resources :plants, only: [:index, :show, :create] 
 end
